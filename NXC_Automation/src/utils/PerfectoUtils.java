25f78d90c5a4a9e8db9bebf3b25ac542daf3a709
avnerg <avnerg@perfectomobile.com>
2015-06-15 15:35:43 +0300

    Create PerfectoUtils.java

---
+++ NXC_Automation/src/utils/PerfectoUtils.java
@@ -0,0 +1,101 @@
+package utils;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.remote.DriverCommand;
+import org.openqa.selenium.remote.RemoteExecuteMethod;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class PerfectoUtils {
+	
+	public static void downloadReport(RemoteWebDriver driver, String type, String fileName) throws IOException {
+		try { 
+			String command = "mobile:report:download"; 
+			Map<String, Object> params = new HashMap<>(); 
+			params.put("type", type); 
+			String report = (String)driver.executeScript(command, params); 
+			File reportFile = new File(fileName + "." + type); 
+			BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(reportFile)); 
+			byte[] reportBytes = OutputType.BYTES.convertFromBase64Png(report); 
+			output.write(reportBytes); output.close(); 
+		} catch (Exception ex) { 
+			System.out.println("Got exception " + ex); }
+	}
+	
+	public static void downloadAttachment(RemoteWebDriver driver, String type, String fileName, String suffix) throws IOException {
+		try {
+			String command = "mobile:report:attachment";
+			boolean done = false;
+			int index = 0;
+
+			while (!done) {
+				Map<String, Object> params = new HashMap<>();	
+
+				params.put("type", type);
+				params.put("index", Integer.toString(index));
+
+				String attachment = (String)driver.executeScript(command, params);
+				
+				if (attachment == null) { 
+					done = true; 
+				}
+				else { 
+					File file = new File(fileName + index + "." + suffix); 
+					BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(file)); 
+					byte[] bytes = OutputType.BYTES.convertFromBase64Png(attachment);	
+					output.write(bytes); 
+					output.close(); 
+					index++; }
+			}
+		} catch (Exception ex) { 
+			System.out.println("Got exception " + ex); 
+		}
+	}
+
+
+	public static void switchToContext(RemoteWebDriver driver, String context) {
+		RemoteExecuteMethod executeMethod = new RemoteExecuteMethod(driver);
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("name", context);
+		executeMethod.execute(DriverCommand.SWITCH_TO_CONTEXT, params);
+	}
+
+	public static String getCurrentContextHandle(RemoteWebDriver driver) {		  
+		RemoteExecuteMethod executeMethod = new RemoteExecuteMethod(driver);
+		String context =  (String) executeMethod.execute(DriverCommand.GET_CURRENT_CONTEXT_HANDLE, null);
+		return context;
+	}
+
+	public static List<String> getContextHandles(RemoteWebDriver driver) {		  
+		RemoteExecuteMethod executeMethod = new RemoteExecuteMethod(driver);
+		List<String> contexts =  (List<String>) executeMethod.execute(DriverCommand.GET_CONTEXT_HANDLES, null);
+		return contexts;
+	}
+	public static boolean checkVisual(RemoteWebDriver driver, String needle){
+		String previousContext = getCurrentContextHandle(driver);
+		// Switch to visual driver, to perform text checkpoint
+		switchToContext(driver, "VISUAL");
+		
+		// Perform the checkpoint
+		try{
+			driver.findElement(By.linkText(needle));
+		}
+		catch(Exception e){
+			switchToContext(driver, previousContext);
+			return false;
+		}
+		
+		// Switch back to webview context
+		switchToContext(driver, previousContext);
+		return true;
+		
+	}
+}
